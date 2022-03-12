9fda7fb3eb2352f5422d92401df3d0718e5a8728
Paul Pernalon <paul.pernalon@gmail.com>
2022-03-12 09:24:16 +0100

    simulation

--- const.py
+++ const.py
@@ -1,5 +1,5 @@
-prix_oeufs_exte = 0.35
-prix_oeuf_inte = 0.20
+prix_oeufs_exte = 0.45
+prix_oeuf_inte = 0.30
 prix_vente_adulte_exte = 4.60
 prix_vente_adulte_inte = 3.50
 prix_vente_vielle_exte = 3.80
@@ -10,3 +10,7 @@ cout_pintade_exte = 0.5
 cout_nouriture = 28 #par an par pintade
 cout_oeuf = 0.02 #par oeuf d'embalage
 cout_veto = 3.5 #une fois dans sa vie quand poussin
+
+budget_initial = 31000 #calculer grace à initial_estimation
+nombre_initial_pintades = 859 #nbre de pintades au bout de deux ans
+nombre_maximal_pintades = 20000
--- main.py
+++ main.py
@@ -1,17 +1,2 @@
 # import const 
 
-
-def suite(u_0, N):
-    u_n=[u_0]
-    for k in range(1, N+1): 
-        if (k<=182):
-            u_n.append(u_n[-1])
-        if (k>182 and k%3==0):
-            u_n.append(u_n[-1] + (u_n[len(u_n)-1-182])//2)
-        else:
-            u_n.append(u_n[-1])
-    return(u_n[-1])
-
-nber_pintades_initial= suite(2,728)
-
-print(nber_pintades_initial)
